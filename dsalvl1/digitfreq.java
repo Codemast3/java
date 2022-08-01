43d279217730447ad7346920065a06818eefc507
Gaurav <gauravbhagat326@gmail.com>
2022-08-02 00:06:14 +0530

    change in code cause of error in digit freq

--- dsalvl1/digitfreq.java
+++ dsalvl1/digitfreq.java
@@ -3,26 +3,30 @@ import java.util.Scanner;
 public class digitfreq {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int d = scn.nextInt();
-        int f = getdigitfrequency(n,d);
+        int n = scn.nextInt();//taking input as n
+        int d = scn.nextInt();//taking digit frequency num as d
+        int f = getdigitfrequency(n,d);//calling function
         System.out.println(f);
 
     }
-    public static getdigitfrequency(int n ,int d ){
-       int rv=0;//return value 
-       whlie(n>0)
+    public static  int getdigitfrequency(int n ,int d ){
+       int rv = 0;//return value 
+       while(n>0) 
        {
         int dig = n % 10; //modulus of n by 10 
         n=n/10;//shortest value of n 
 
         
-        if(dig == d ){
+        if(dig == d )//logic applied
             rv++;
 
+       
         }
-       }
-     }
+       
+       return rv;
+    }
 }
+
+
 
 
