dc0b73970fd6b0ee67356b9ea83bb0a3c6e43dbd
Gaurav <gauravbhagat326@gmail.com>
2022-10-14 22:50:02 +0530

    insert node

---
+++ linkedlist/insertnod.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class insertnod {
+
+    public static Node<Integer> takeinput() {
+        Node<Integer> head = null;
+        Scanner scn = new Scanner(System.in);
+        int data = scn.nextInt();
+
+
+        while(data != -1){
+            Node<Integer> newNode = new Node<Integer>(data);
+            if(head == null){
+                head = newNode;
+
+            }else{
+               Node<Integer> temp = head;
+               while(temp.next!=null){
+                temp = temp.next;
+                }
+                temp.next = newNode;
+
+            }
+
+            data =scn.nextInt();
+        }
+        return head;
+
+    }
+    public static void print(Node<Integer> head){
+        Node<Integer> temp = head;
+        while(head != null){
+        System.out.print(head.data + " ");
+        head = head.next;
+        }
+        System.out.println();
+        /*head = temp;
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+            }*/
+    
+       } 
+         public static void main(String[] args) {
+            
+        
+           // Node<Integer> node1 = new Node<Integer>(10);//first node 
+           // System.out.println(node1.data); 
+          
+           // System.out.println(node3);//address of node 2
+            //System.out.println(node3.next);
+            Node<Integer> head = takeinput();
+            print(head);
+            //print(head);
+        
+        }
+    
+}
