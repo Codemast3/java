3c5de8e4254ea26a7b987ed247b035791a702d2f
Gaurav <gauravbhagat326@gmail.com>
2022-10-17 21:29:21 +0530

    insertion in linked list

--- linkedlist/linkinsert.java
+++ linkedlist/linkinsert.java
@@ -1,3 +1,75 @@
+import java.util.Scanner;
 public class linkinsert {
+    public static Node<Integer> takeinput(){
+         
+            Node<Integer> head = null, tail =null;
+            Scanner scn = new Scanner(System.in);
+            int data = scn.nextInt();  
+            
     
-}
+    
+            while(data != -1){
+                Node<Integer> newNode = new Node<Integer>(data);
+                if(head == null){
+                    head = newNode;
+                    tail= newNode; // tail = tail.next
+    
+                }else{
+                   Node<Integer> temp = head;
+                   while(temp.next!=null){
+                    temp = temp.next;
+                    }
+                    temp.next = newNode;
+    
+                }
+    
+                data = scn.nextInt();
+            }
+            return head;
+    }
+
+    public static void print(Node<Integer> head){
+        Node<Integer> temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data +" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+        
+
+        public static void insert(Node<Integer> head, int data,  int pos ) {
+        Node<Integer> newnode = new Node<Integer>(data);
+        if(pos == 0){
+            newnode.next = head;
+            head = newnode;
+            return;
+            
+
+        }
+        int i = 0;
+        Node<Integer> temp = head;
+        while(i < pos - 1){
+            temp = temp.next;
+            i++;
+        }
+        newnode.next = temp.next;
+        temp.next = newnode;
+
+        }
+    
+        public static void main(String[] args) {
+            Node<Integer> head = takeinput();
+            insert(head,80,3);
+            print(head);
+        }
+    }
+            
+        
+
+    
+
+    
+
