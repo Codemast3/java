060838b1698ee0ca963d1f94542d10c4df9b17fe
gaurav bhagat <gauravbhagat326@gmail.com>
2022-11-04 21:07:08 +0530

    fraction operations

--- oops/studentuse_3267/fractionuse.java
+++ oops/studentuse_3267/fractionuse.java
@@ -1,5 +1,4 @@
-package studentuse_3267;
-import studentuse_3267.fraction;
+
 
 public class fractionuse {
     public static void main(String[] args) {
@@ -8,8 +7,25 @@ public class fractionuse {
         f1.setnumerator(12);
             int d = f1.getdenominator();
             System.out.println(d);
-            f1.print();
-        }
+        f1.print();
+        
+        f1.setnumerator(10);
+        f1.setdenominator(30);
+        f1.print();
+
+
+        //add program of fractions
+        fraction f2 = new fraction(3, 4);
+        f1.add(f2);
+        f1.print();
+        f2.print();
+
+        // third object for multiplication of fractions
+        fraction f3 = new fraction(4, 5);
+        f3.multiply(f2);
+        f3.print();
+        f2.print();
     }
+}
     
 
