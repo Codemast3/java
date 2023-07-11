503ef6ef959d34e4e466e11c0b655cbc19c68110
Gaurav <gauravbhagat326@gmail.com>
2023-07-11 23:52:54 +0530

    div 1 prblm

---
+++ codechef/twothnotes.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+
+public class twothnotes {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println((n*2000)/500);
+    }
+    
+}
