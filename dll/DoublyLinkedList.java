b02177ded5d357103a4667658cb95c46f33298e9
Gaurav <gauravbhagat326@gmail.com>
2024-09-27 02:11:54 +0530

    doubly link list and operations

---
+++ dll/DoublyLinkedList.java
@@ -0,0 +1,89 @@
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public DoublyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void remove(int data) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
+                if (current.prev != null) {
+                    current.prev.next = current.next;
+                } else {
+                    head = current.next;
+                }
+                if (current.next != null) {
+                    current.next.prev = current.prev;
+                } else {
+                    tail = current.prev;
+                }
+                size--;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public void fromArray(int[] array) {
+        for (int data : array) {
+            add(data);
+        }
+    }
+
+    private static class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        
+        // Example array
+        int[] array = {1, 2, 3, 4, 5};
+        
+        // Populate the list from the array
+        list.fromArray(array);
+        
+        // Print the list
+        list.print();
+        
+        // Remove an element and print again
+        list.remove(3);
+        list.print();
+    }
+}
